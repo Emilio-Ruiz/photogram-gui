2880e0ead8f62e99bfe0bd6f92dad68ee75638b6
Emilio Ruiz <ruiz.emilio144@gmail.com>
2023-03-28 19:16:12 +0000

    Photo create works!

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -33,4 +33,23 @@ class PhotosController < ApplicationController
 
     redirect_to("/photos")
   end
+
+  def create
+     #Parameters: {"query_image"=>"http://www.chicagobooth.edu/en/why-booth/stories/hub-stories/2023/february/building-up-leaders-of-color", "query_caption"=>"Chicago Booth", "query_owner_id"=>"117"}
+
+    input_image = params.fetch("query_image")
+    input_caption = params.fetch("query_caption")
+    input_owner_id = params.fetch("query_owner_id")
+
+    a_new_photo = Photo.new 
+    a_new_photo.image = input_image 
+    a_new_photo.caption = input_caption
+    a_new_photo.owner_id = input_owner_id 
+
+    a_new_photo.save 
+
+    render({:template => "photo_templates/create.html.erb"})
+
+    #redirect_to("/photos/" + a_new_photo.id.to_s)
+  end
 end
