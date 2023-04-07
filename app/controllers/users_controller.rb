e0a4dba89b0eda843ff92eae9c14c89acb8405b2
Emilio Ruiz <ruiz.emilio144@gmail.com>
2023-04-07 14:54:41 +0000

    git

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     matching_users = User.all
     @list_of_users = matching_users.order({ :username => :asc })
     render({ :template => "user_templates/index.html.erb" })
+
   end
 
   def show
