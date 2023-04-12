1eb5c3368790bce9278ec85c03c5ed15e0ea8018
Emilio Ruiz <ruiz.emilio144@gmail.com>
2023-04-12 19:24:54 +0000

    newest git

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,42 +1,11 @@
-class UsersController < ApplicationController 
+class UsersController < ApplicationController
   def index 
-    matching_users = User.all 
-   @list_of_users = matching_users.order({:username => :asc})
-    render({:template => "user_templates/index.html.erb"})
-  end
-  def show 
 
-    #Parameters: {"path_username" => "anisa"}
-
-   url_username = params.fetch("path_username")
+    matching_users =  User.all 
 
 
-    matching_usernames = User.where({ :username => url_username })
 
-    @the_user = matching_usernames.at(0)
-
-    #if the_user == nil 
-    #  redirect_to("/")
-    #else 
-      #render({:template => "user_templates/show.html.erb"})
-
-   # end
-    render({:template => "user_templates/show.html.erb"})
-  end
-  def update
-    input_user = params.fetch("username")
-
-  end
-  def create 
-    #{"path_username"=>"cristina"}
-
-  input_user = params.fetch("input_username")
-
-    a_new_user = User.new
-    a_new_user.username = input_user 
-    a_new_user.save
-
-
-    render({:template => "user_templates/create.html.erb"})
+    @list_of_users = matching_users.order({:username => :asc})
+    render({ :template => "user_templates/index.html.erb"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,3 @@
 Rails.application.routes.draw do
-
   get("/users", {:controller => "users", :action => "index"})
-  get("/users/:path_username", {:controller => "users", :action => "show"})
-  get("/photos/", {:controller => "photos", :action => "index"})
-  get("photos/:path_id", {:controller => "photos", :action => "show"})
-
-  get("/delete_photo/:toast_id", {:controller => "photos", :action => "baii"})
-  get("/insert_photo", {:controller => "photos", :action => "create"})
-  get("/update_photo/:modify_id", {:controller => "photos", :action => "update" })
-  get("/", {:controller => "photos", :action => "index"})
-  get("/insert_user_record", {:controller => "users", :action => "create"})
 end
