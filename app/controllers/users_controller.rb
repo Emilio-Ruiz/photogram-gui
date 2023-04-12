fa9a480f08f036b655f4fc1a96c5e2ee2e77876a
Emilio Ruiz <ruiz.emilio144@gmail.com>
2023-04-12 20:01:15 +0000

    recently git

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,18 @@ class UsersController < ApplicationController
     matching_users =  User.all 
 
 
-
     @list_of_users = matching_users.order({:username => :asc})
+
     render({ :template => "user_templates/index.html.erb"})
+  end
+  def show 
+
+    #Parameters: {"path_username"=>"anisa"}
+    url_username = params.fetch("path_username")
+
+    matching_usernames = User.where({:username => url_username})
+
+    @the_user = matching_usernames.at(0)
+
   end
 end
